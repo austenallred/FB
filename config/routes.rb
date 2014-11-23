982521b1dc329055cb7f73bcf5b3b47605a52761
Austen Allred <austen.allred@gmail.com>
2014-11-23 00:14:01 -0700

    create basic site structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/help'
+
+  get 'pages/about'
+
   resources :users
+
+  root 'pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
